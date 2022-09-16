e927106167cc3a11ca06f3a1d680883a4d356eac
James Lee <j.lee1203@hotmail.com>
2022-09-15 20:20:59 -0700

    commit tree 770bf05b6752e6576e8f2a2729bb5e9836e7fc85

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++){
+    // n steps
+    const complement = target - array[i]
+    // making complement variable but just 1 step per iteration
+    for (let j = i + 1; j < array.length; j++){
+      // because of a nested loop, its n * n steps
+      if (array[j] === complement) return true;
+    }
+  }
+  return false;
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  Runtime is quadratic time or O(n^2)
 */
 
 /* 
-  Add your pseudocode here
+  iterating through an array of integers
+    add the current integer to each integer in the array
+      if int a and int b = target integer
+        return true
+      if no int a + int b = target integer
+        return false
 */
 
 /*
-  Add written explanation of your solution here
+  Iterating through the array and defined a complement which is target - the current integer of the array 
+  iteration. a second for loop is coded to serve as the other loop happening simultaenously at the +1 so it's
+  always comparing 2 different integers.
+  finally if the array[j] is the same as the complement, then we know its true.
 */
 
 // You can run `node index.js` to view these console logs
